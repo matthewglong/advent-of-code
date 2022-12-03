c89867adb39f473003f14549dd36ff9a62365842
Matthew Long <32203334+matthewglong@users.noreply.github.com>
2022-12-03 01:03:21 -0800

    Hmmm what should I put here.... Sometimes it is difficult to decide. I suppose I could just go with xxx or something simple. Yeah, I like that idea. Let's do tht.

---
+++ 2022/Abel/day_3/day_3.py
@@ -0,0 +1,26 @@
+with open('data.txt') as f:
+    data = []
+    for line in f:
+        line = line.rstrip()
+        compartment_1 = set(list(line[:int(len(line)/2)]))
+        compartment_2 = set(list(line[int(len(line)/2):]))
+        data.append([compartment_1, compartment_2])
+
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+total = 0
+for bag in data:
+    item = bag[0].intersection(bag[1]).pop()
+    score = alphabet.index(item) + 1
+    total += score
+print(total)
+
+total = 0
+for index in range(0, len(data), 3):
+    rucksack_1 = data[index][0].union(data[index][1])
+    rucksack_2 = data[index + 1][0].union(data[index + 1][1])
+    rucksack_3 = data[index + 2][0].union(data[index + 2][1])
+    badge = rucksack_1.intersection(rucksack_2, rucksack_3).pop()
+    score = alphabet.index(badge) + 1
+    total += score
+print(total)
