2d6c4733e033f25b13d425a5acdab52fca616096
Matthew Long <32203334+matthewglong@users.noreply.github.com>
2022-12-05 10:28:34 -0800

    refactored 1 (to support 2) and completed 2

--- 2022/Matthew/day05/day05.py
+++ 2022/Matthew/day05/day05.py
@@ -17,27 +17,36 @@ class Day5:
         self.stacks = {s[-1]: list(''.join(s[:-1]).strip()[::-1])
                        for s in decrufted}
 
+        self.proc_version = {
+            9000: self.proc_step_9000, 9001: self.proc_step_9001}
+
         # Parse the unloading procedure
         raw_proc = d[partition+1:]
         self.proc = list(map(lambda x: re.findall(r'\d+', x), raw_proc))
 
-    def proc_step(self, command):
+    def proc_step_9000(self, command, stacks):
         for _ in range(int(command[0])):
-            crate = self.stacks[command[1]].pop()
-            self.stacks[command[2]].append(crate)
+            crate = stacks[command[1]].pop()
+            stacks[command[2]].append(crate)
 
-    def rearrange(self):
+    def proc_step_9001(self, command, stacks):
+        stacks[command[2]] += stacks[command[1]][-int(command[0]):]
+        del stacks[command[1]][-int(command[0]):]
+
+    def rearrange(self, stacks, proc):
         for command in self.proc:
-            self.proc_step(command)
+            proc(command, stacks)
 
-    def top_of_stacks(self):
-        self.rearrange()
-        tops = ''.join([s[-1] for s in self.stacks.values()])
-        print(f'Stack tops are {tops}')
+    def top_of_stacks(self, v):
+        stacks = {k: [t for t in v]
+                  for k, v in self.stacks.items()}  # Deep copy the data
+        proc = self.proc_version[v]
+        self.rearrange(stacks, proc)
+        tops = ''.join([s[-1] for s in stacks.values() if len(s) > 0])
+        print(f'Stack tops with the CrateMover {v} are {tops}')
         return tops
 
 
-# %%
 d = Day5()
-d.top_of_stacks()
-# %%
+d.top_of_stacks(9000)
+d.top_of_stacks(9001)
