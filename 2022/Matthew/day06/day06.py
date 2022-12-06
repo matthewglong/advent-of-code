ca11d1757c37155e80034f9b49539a9b46fbcb25
Matthew Long <32203334+matthewglong@users.noreply.github.com>
2022-12-05 23:24:33 -0800

    completed day 6

---
+++ 2022/Matthew/day06/day06.py
@@ -0,0 +1,22 @@
+class Day6:
+    def __init__(self):
+        with open('day06.txt', 'r') as f:
+            self.datastream = f.read().strip()
+
+    def find_marker(self, size=4):
+        count = size
+        marker = self.datastream[:size]
+        remaining_data = self.datastream[size:]
+        for char in remaining_data:
+            if len(set(marker)) == size:
+                print(f'{marker} @ {count} for a {size} char marker')
+                return marker, count
+            else:
+                marker = marker[1:] + char
+                count += 1
+        return None, -1
+
+
+d = Day6()
+d.find_marker(4)
+d.find_marker(14)
