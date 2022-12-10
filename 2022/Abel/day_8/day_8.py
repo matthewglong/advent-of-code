8ed9d755957266b86dd5db6614762b7ee5e35a30
Abel Romer <abel.romer@questu.ca>
2022-12-09 23:37:50 -0800

    bleh

--- 2022/Abel/day_8/day_8.py
+++ 2022/Abel/day_8/day_8.py
@@ -1,36 +1,45 @@
-import numpy as np
-
-
 data = '94651298688456123161881394547336912458971234908712398471928347923847298376'
 data2 = '12345654321'
 cleaned = list(map(int, list(data)))
-print(cleaned)
 counter = 0
+
 
 class TreeCounter():
     def __init__(self):
         with open('data.txt', 'r') as f:
-            list(map(lambda x: list(map(int, list(x))), f.readlines()))
+            self.data = list(map(lambda x: list(map(int, list(x))), f.read().splitlines()))
 
-def count_visible(trees, counter):
-    counter += 1
-    finished = True
-    to_remove = []
-    for idx in range(1, len(trees) - 1):
-        if trees[idx] <= trees[idx - 1] and trees[idx] <= trees[idx + 1]:
-            to_remove.append(idx)
-            finished = False
-    for i in sorted(to_remove, reverse=True):
-        del trees[i]
-    if finished:
-        return trees, counter
-    return count_visible(trees, counter)
+    def count_visible(self, trees, indices):
+        finished = True
+        to_remove = []
+        for idx in range(1, len(trees) - 1):
+            if trees[idx] <= trees[idx - 1] and trees[idx] <= trees[idx + 1]:
+                to_remove.append(idx)
+                finished = False
+        for i in sorted(to_remove, reverse=True):
+            del trees[i]
+            indices.pop(i)
+        if finished:
+            return trees, indices
+        return self.count_visible(trees, indices)
 
-iterations = []
+    def get_visible_trees(self):
+        trees = []
+        inverted_data = list(map(list, zip(*self.data)))
+        print(inverted_data[0])
+        for idx, row in enumerate(inverted_data):
+            t, i = self.count_visible(row, list(range(len(row))))
+            trees += list(map(lambda x: (idx, x), i))
+        print(len(trees))
+        for idx, row in enumerate(self.data):
+            t, i = self.count_visible(row, list(range(len(row))))
+            trees += list(map(lambda x: (x, idx), i))
+        print(len(trees))
+        print(len(list(set(trees))))
+        return len(list(set(trees)))
 
-for _ in range(100):
-    randomlist = list(np.random.choice(range(1, 10), 1000, replace=True))
-    visible_trees, c = count_visible(randomlist, 0)
-    iterations.append(c)
 
-print(max(iterations))
+tc = TreeCounter()
+print(tc.data)
+#tc.count_visible(cleaned, list(range(len(cleaned))))
+tc.get_visible_trees()
