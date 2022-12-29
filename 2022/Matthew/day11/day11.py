9054e3e5258b5ac1915170b693e6fdc5513cd627
Matthew Long <32203334+matthewglong@users.noreply.github.com>
2022-12-29 09:59:30 -0800

    part 2

--- 2022/Matthew/day11/day11.py
+++ 2022/Matthew/day11/day11.py
@@ -1,5 +1,6 @@
 # %%
 import re
+import math
 
 
 class Day11:
@@ -9,38 +10,57 @@ class Day11:
         with open('day11.txt', 'r') as f:
             monkeys = {}
             i = 0
+            mod = 1
             for raw_info in f.read().split('\n\n'):
                 self.curr = raw_info
                 i += 1
                 info = regex.search(raw_info).groups()
                 m = Monkey(info)
                 monkeys[m.name] = m
+                mod *= m.test_num
         self.monkeys = monkeys
+        self.mod = mod
 
-    def parse_monkey(self, monkey):
+    def echo_monkeys(self, round):
+        if round:
+            print(f'\nROUND {round}')
+        for _, v in self.monkeys.items():
+            print(v, v.items)
+
+    def parse_monkey(self, monkey, worry_reduction):
         for item in monkey.items:
             monkey.items_inspected += 1
             worry = monkey.inspect(item)
-            reduced_worry = int(worry/3)
+            if worry_reduction:
+                reduced_worry = worry // worry_reduction
+            else:
+                reduced_worry = worry % self.mod
             if reduced_worry % monkey.test_num == 0:
                 self.monkeys[monkey.true_monkey].items.append(reduced_worry)
             else:
                 self.monkeys[monkey.false_monkey].items.append(reduced_worry)
             monkey.items = []
 
-    def parse_monkeys(self):
+    def parse_monkeys(self, worry_reduction):
         for _, monkey in self.monkeys.items():
-            self.parse_monkey(monkey)
+            self.parse_monkey(monkey, worry_reduction)
 
-    def monkeying_around(self, rounds=20, top_n=2):
+    def monkeying_around(self, rounds=20, top_n=2, worry_reduction=None, test_points=None):
         for i in range(rounds):
-            self.parse_monkeys()
+            if test_points:
+                if i in test_points:
+                    self.echo_monkeys(i)
+            self.parse_monkeys(worry_reduction=worry_reduction)
+            i += 1
+        if test_points:
+            if i in test_points:
+                self.echo_monkeys(i)
 
         inspections = [m.items_inspected for _, m in self.monkeys.items()]
         out = 1
         for num in sorted(inspections)[-top_n:]:
             out *= num
-        print(f"Answer key is: {out}")
+        print(f"Monkeying around value: {out}")
 
 
 class Monkey:
@@ -66,7 +86,6 @@ class Monkey:
             self.inspect = self.inspect_item_self
 
     def __repr__(self):
-        # return f'Monkey {self.name} (opp: {self.opp_label} {self.opp_num}; test: by {self.test_num})'
         return f'Monkey {self.name} inspected {self.items_inspected} items'
 
     def inspect_item_old(self, item):
@@ -78,6 +97,7 @@ class Monkey:
 
 # %%
 d = Day11()
-d.monkeying_around()
+d.monkeying_around(rounds=20, worry_reduction=3)
 
-# %%
+d = Day11()
+d.monkeying_around(rounds=10_000, test_points=(1, 10_000))
